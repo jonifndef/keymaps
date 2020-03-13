b7cab98e3941aabbb756c9654acc0b2638c903ec
Jonas Fredriksson <jonas.fredriksson@cind.se>
2020-03-13 08:38:28 +0100

    Change KC_NUBS, del, mins, add media layer

--- xd60/keymap.c
+++ xd60/keymap.c
@@ -5,12 +5,12 @@
 #define _FL 1
 #define _GB 2
 #define _GF 3
-#define _ML 4 
+#define _ML 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	
+
 	// 0: Base Layer
-  [_BL] = LAYOUT_all( 
+  [_BL] = LAYOUT_all(
       KC_ESC,           KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_NO,   \
       KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_NO,   \
       MO(_FL),          KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NUHS,           KC_ENT,  \
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                          KC_RGUI, F(0),   KC_NO,   KC_PGDOWN, KC_NO),
 
 */
-};  	
+};
 
 // Custom Actions
 const uint16_t PROGMEM fn_actions[] = {
