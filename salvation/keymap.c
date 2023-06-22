112379a21c010c0262ab42a60b1b500375a616ee
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2023-06-22 13:22:54 +0200

    fixup! Add salvation with wt60_d pcb

--- salvation/keymap.c
+++ salvation/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
 	KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 	KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TG(2),
-	KC_LCTL, KC_LGUI, KC_LALT,                            LT(1, KC_SPC),                      KC_RALT, KC_NO,   KC_RGUI, KC_RCTL),
+	KC_LCTL, KC_LGUI, KC_LALT,                            LT(1, KC_SPC),                      KC_NO,   KC_RALT, KC_RGUI, KC_RCTL),
 
 // Fn1 Layer
 [1] = LAYOUT_all(
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
 	MO(3),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 	KC_LSFT, KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_TRNS,
-	KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_NO,   KC_RGUI, KC_RCTL),
+	KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_NO,   KC_RALT,   KC_RGUI, KC_RCTL),
 
 // Fn3 Layer
 [3] = LAYOUT_all(
